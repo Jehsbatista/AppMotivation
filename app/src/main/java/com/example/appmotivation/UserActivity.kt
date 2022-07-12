aa4da369d924beabc1567c6f1eb2b3ae3d0ed7c3
jessica.batista <jessica.batista@g.globo>
2022-07-12 20:51:45 -0300

    styling the UserActivity and implementing the click event for the save button

--- app/src/main/java/com/example/appmotivation/UserActivity.kt
+++ app/src/main/java/com/example/appmotivation/UserActivity.kt
@@ -2,10 +2,25 @@ package com.example.appmotivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.appmotivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(),View.OnClickListener{
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save){
+
+        }
     }
 }

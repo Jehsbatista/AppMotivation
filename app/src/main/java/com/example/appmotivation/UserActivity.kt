ce7c36c3803efded6a6442e531b48ee8b6dd731d
jessica.batista <jessica.batista@g.globo>
2022-07-14 14:06:40 -0300

    Intent implementation for navigating between activities

--- app/src/main/java/com/example/appmotivation/UserActivity.kt
+++ app/src/main/java/com/example/appmotivation/UserActivity.kt
@@ -1,11 +1,13 @@
 package com.example.appmotivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.appmotivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity(),View.OnClickListener{
+class UserActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityUserBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +21,19 @@ class UserActivity : AppCompatActivity(),View.OnClickListener{
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_save){
+        if (v.id == R.id.button_save) {
+            handleSave()
 
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_LONG).show()
         }
     }
 }

197ef609ecb185ebca2a5c12851e892ed880ba4c
jessica.batista <jessica.batista@g.globo>
2022-07-15 14:08:40 -0300

    implementing the SecurityPreferences class and its methods

---
+++ app/src/main/java/com/example/appmotivation/SecurityPreferences.kt
@@ -0,0 +1,19 @@
+package com.example.appmotivation
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SecurityPreferences(context: Context) {
+    private val security: SharedPreferences =
+        context.getSharedPreferences("Motivation", Context.MODE_PRIVATE)
+
+    fun storeString(Key: String, str: String) {
+        security.edit().putString(Key, str).apply()
+    }
+
+    fun getString(Key: String): String {
+        return security.getString(Key, "") ?: ""
+
+    }
+
+}

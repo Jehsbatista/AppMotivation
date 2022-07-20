f3baa2bf664feff6128d8572d923bd42337ee9fd
jessica.batista <jessica.batista@g.globo>
2022-07-20 19:09:07 -0300

    adding colors to the filter

--- app/src/main/java/com/example/appmotivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/appmotivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appmotivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.appmotivation.infra.MotivationConstants
 import com.example.appmotivation.R
 import com.example.appmotivation.infra.SecurityPreferences
@@ -25,11 +26,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         //eventos
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
+
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+
+            }
         }
     }
 
